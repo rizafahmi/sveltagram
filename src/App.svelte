99083df1c1bc6c61a6308fb184fb0ecb63b7b37a
Riza Fahmi <rizafahmi@gmail.com>
2020-04-28 13:18:20 +0700

    Iterate PhotoList

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
   import BigHeader from './components/BigHeader.svelte';
+  import PhotoList from './components/PhotoList.svelte';
+
   export let hashtag;
 </script>
 
 <main>
   <BigHeader hashtag={hashtag} />
   <input type="text" bind:value={hashtag} />
+  <PhotoList />
 </main>
 
 <style>
---
+++ src/components/PhotoList.svelte
@@ -0,0 +1,16 @@
+<script>
+  import data from '../instagram.json';
+  const photos = data.graphql.hashtag.edge_hashtag_to_media.edges;
+</script>
+
+<div>
+  <ul>
+    {#each photos as photo}
+      <li>
+        <img src="{photo.node.thumbnail_src}" alt="thumbnail" />
+        <p>{photo.node.edge_media_to_caption.edges[0].node.text}</p>
+      </li>
+    {/each}
+  </ul>
+</div>
+<style></style>

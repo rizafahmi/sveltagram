c5f9241bc08b29931db1968e82e10b6f17bf7f39
Riza Fahmi <rizafahmi@gmail.com>
2020-04-28 10:56:32 +0700

    Demo nested component

---
+++ src/components/BigHeader.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let hashtag;
+</script>
+
+<h1>#{hashtag}</h1>
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
